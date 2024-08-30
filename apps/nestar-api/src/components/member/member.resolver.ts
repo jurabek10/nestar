cf731dc332559997f5cdb4532ca50401bc4a621a
jurabek10 <samiev.jurabek@bk.ru>
2024-08-31 00:10:07 +0900

    fix: modify updateMember api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -7,8 +7,9 @@ import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
-import { MemberType } from '../../libs/enums/member.enum';
+import { MemberStatus, MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Resolver()
 export class MemberResolver {
@@ -32,16 +33,6 @@ export class MemberResolver {
 		}
 	}
 
-	// Authenticated
-	@UseGuards(AuthGuard)
-	@Mutation(() => String)
-	public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
-		console.log('Mutation: updateMember');
-		// console.log(typeof memberId);
-		// console.log(memberId);
-		return this.memberService.updateMember();
-	}
-
 	@UseGuards(AuthGuard)
 	@Query(() => String)
 	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
@@ -61,6 +52,18 @@ export class MemberResolver {
 		// console.log(typeof memberId);
 		// console.log(memberId);
 		return `Hi ${authMember.memberNick} you are ${authMember.memberType} (memberId: ${authMember._id})`;
+	}
+
+	// Authenticated
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async updateMember(
+		@Args('input') input: MemberUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation: updateMember');
+		delete input._id;
+		return this.memberService.updateMember(memberId, input);
 	}
 
 	@Query(() => String)
