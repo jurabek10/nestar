5d9eed99943b258c619f845958bfefe9b33bdb8a
jurabek10 <samiev.jurabek@bk.ru>
2024-09-08 22:53:45 +0900

    feat: add dto of boardArticle module

--- apps/nestar-api/src/components/board-article/board-article.module.ts
+++ apps/nestar-api/src/components/board-article/board-article.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { BoardArticleResolver } from './board-article.resolver';
+import { BoardArticleService } from './board-article.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import BoardArticleSchema from '../../schemas/BoardArticle.model';
+import { AuthModule } from '../auth/auth.module';
 
-@Module({})
+@Module({
+	imports: [MongooseModule.forFeature([{ name: 'BoardArticle', schema: BoardArticleSchema }]), AuthModule],
+	providers: [BoardArticleResolver, BoardArticleService],
+})
 export class BoardArticleModule {}
