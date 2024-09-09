737ede211e408613e415f69b29f7fba8a6167167
jurabek10 <samiev.jurabek@bk.ru>
2024-09-10 00:11:37 +0900

    feat: develop createArticle and getArticle apis

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -1,9 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { BoardArticle } from '../../libs/dto/board-article/board-article';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { MemberService } from '../member/member.service';
+import { ViewService } from '../view/view.service';
+import { Message } from '../../libs/enums/common.enum';
+import { BoardArticleStatus } from '../../libs/enums/board-article.enum';
+import { StatisticModifier, T } from '../../libs/types/common';
+import { ViewGroup } from '../../libs/enums/view.enum';
 
 @Injectable()
 export class BoardArticleService {
-	constructor(@InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>) {}
+	constructor(
+		@InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>,
+		private readonly memberService: MemberService,
+		private readonly viewService: ViewService,
+	) {}
+
+	public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
+		input.memberId = memberId;
+		try {
+			const result = await this.boardArticleModel.create(input);
+			await this.memberService.memberStatsEditor({ _id: memberId, targetKey: 'memberArticles', modifier: 1 });
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATED_FAILED);
+		}
+	}
+
+	public async getBoardArticle(memberId: ObjectId, articleId: ObjectId): Promise<BoardArticle> {
+		const search: T = {
+			_id: articleId,
+			articleStatus: BoardArticleStatus.ACTIVE,
+		};
+
+		const targetBoardArticle: BoardArticle = await this.boardArticleModel.findOne(search).lean().exec();
+
+		if (!targetBoardArticle) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		if (memberId) {
+			const viewInput = { memberId: memberId, viewRefId: articleId, viewGroup: ViewGroup.ARTICLE };
+			const newView = await this.viewService.recordView(viewInput);
+			if (newView) {
+				await this.boardArticleStatsEditor({ _id: articleId, targetKey: 'articleViews', modifier: 1 });
+				targetBoardArticle.articleViews++;
+			}
+		}
+
+		targetBoardArticle.memberData = await this.memberService.getMember(null, targetBoardArticle.memberId);
+		return targetBoardArticle;
+	}
+
+	public async boardArticleStatsEditor(input: StatisticModifier): Promise<BoardArticle> {
+		console.log('boardArticleStatsEditor executed');
+		const { _id, targetKey, modifier } = input;
+		return await this.boardArticleModel
+			.findByIdAndUpdate(_id, { $inc: { [targetKey]: modifier } }, { new: true })
+			.exec();
+	}
 }
