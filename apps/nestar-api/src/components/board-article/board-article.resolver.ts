781215e7b6dd53ed50e67fcf0cd33bf25ea08fb6
jurabek10 <samiev.jurabek@bk.ru>
2024-09-09 23:07:15 +0900

    feat: configure board service and board article resolver

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -1,4 +1,7 @@
 import { Resolver } from '@nestjs/graphql';
+import { BoardArticleService } from './board-article.service';
 
 @Resolver()
-export class BoardArticleResolver {}
+export class BoardArticleResolver {
+	constructor(private readonly boardArticleService: BoardArticleService) {}
+}
