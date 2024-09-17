e5c6168315587daa2f85c708711ab3033420d289
jurabek10 <samiev.jurabek@bk.ru>
2024-09-18 02:12:24 +0900

    feat: develop getFavorites api

--- apps/nestar-api/src/libs/dto/property/property.input.ts
+++ apps/nestar-api/src/libs/dto/property/property.input.ts
@@ -236,3 +236,16 @@ export class AllPropertiesInquiry {
 	@Field(() => ALPSearch)
 	search: ALPSearch;
 }
+
+@InputType()
+export class OrdinaryInquiry {
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	page: number;
+
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	limit: number;
+}
