28fcebfb0a7385eb0dd82532c96dd3c3d1e2a6ee
jurabek10 <samiev.jurabek@bk.ru>
2024-09-19 02:04:14 +0900

    feat: create batchRollback, batchProperties and batchAgents

--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BatchService {
-	public getHello(): string {
-		return 'Welcome to Nestar BATCH Server!';
-	}
-
 	public async batchRollback(): Promise<void> {
 		console.log('batchRollback');
 	}
@@ -16,5 +12,9 @@ export class BatchService {
 
 	public async batchAgents(): Promise<void> {
 		console.log('batchAgents');
+	}
+
+	public getHello(): string {
+		return 'Welcome to Nestar BATCH Server!';
 	}
 }
